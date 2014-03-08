8454abc6cda9164e75d741a92546f8dc809691ce
unknown <couderc.antoine@gmail.com>
2014-03-08 14:51:51 +0100

    somehow ok

--- client/swxclient.js
+++ client/swxclient.js
@@ -1,13 +1,37 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "The Missing Meteor Tutorial";
-  };
+  Questions = new Meteor.Collection("questions");
+  
+  Deps.autorun(function() {
+    Meteor.subscribe('userData');
+    console.log(Meteor.user());
+  });
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.linkCV.events({
+    'click input.link-cv' : function(event){
+      console.log('ok');
+      event.preventDefault();
+      var cv = document.getElementById("linkCVtext").value;
+      Meteor.call("addCV", cv);
     }
   });
+
+  Template.addquestion.events({
+    'click input.add-question' : function(event){
+      event.preventDefault();
+      var questionText = document.getElementById("questionText").value;
+      Meteor.call("addQuestion", questionText);
+      document.getElementById("questionText").value = "";
+    }
+  });
+
+  Template.questions.items = function(){
+    return Questions.find({},{sort:{'submittedOn':-1}});
+  };
+
+  Template.linkCV.voidcv = function(){
+    return Meteor.user().cv === '';
+  }
+
+  
 }
+
--- server/swxserver.js
+++ server/swxserver.js
@@ -1,5 +1,36 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Questions = new Meteor.Collection("questions");
+
+	
+
+	Meteor.startup(function () {
+		Meteor.publish("userData", function()
+		{
+			return Meteor.users.find({_id : this.userId},
+								{fields: {'cv':1}});
+		});
+	});
+
+	Accounts.onCreateUser(function(options, user){
+		user.cv = "";
+		if (options.profile)
+			user.profile = options.profile;
+		return user;
+	});
+
+	Meteor.methods({
+		addQuestion : function(questionText){
+			var questionId = Questions.insert({
+				'questionText' : questionText,
+				'submittedOn': new Date()
+			});
+			return questionId;
+		},
+		addCV : function(linkCVtext){
+			Meteor.users.update({_id : Meteor.userId()},
+						{$set: {cv:linkCVtext}});
+			console.log(Meteor.user());
+		}	
+	})
+
 }

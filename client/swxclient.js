eef08f825ceb9a273a42ae0042206e4f65f9a037
acouderc <couderc.antoine@gmail.com>
2014-03-08 21:16:05 +0100

    Doesnt work

--- client/swxclient.js
+++ client/swxclient.js
@@ -1,14 +1,27 @@
+Colors = new Meteor.Collection("colors");
+
 if (Meteor.isClient) {
+  Students = new Meteor.Collection("Students");
+
+  Template.color_list.colors = function()
+  {
+    return Colors.find({}, {sort: {name:1}});
+  }
+  Template.Profils.items = function()
+  {
+    console.log('ok');
+    return Students.find({}, {sort: {name : 1}});
+  }
+
   Questions = new Meteor.Collection("questions");
   
-  Deps.autorun(function() {
+  Deps.autorun(function()
+  {
     Meteor.subscribe('userData');
-    console.log(Meteor.user());
   });
 
   Template.linkCV.events({
     'click input.link-cv' : function(event){
-      console.log('ok');
       event.preventDefault();
       var cv = document.getElementById("linkCVtext").value;
       Meteor.call("addCV", cv);
@@ -28,10 +41,44 @@ if (Meteor.isClient) {
     return Questions.find({},{sort:{'submittedOn':-1}});
   };
 
+  Template.menu.events({
+      'click a' : function(event){
+        event.preventDefault();
+        var choice = event.toElement.id;
+        switch(choice)
+        {
+          case 'quizz-creation':
+            Meteor.call('changePos', 'quizzcreation');
+            break;
+          case 'quizz':
+            Meteor.call('changePos', 'quizz');
+            break;
+        }
+      }
+  });
+
   Template.linkCV.voidcv = function(){
     return Meteor.user().cv === '';
   }
 
-  
+  Template.main.welcome = function(){
+    return Meteor.user().position == 'welcome';
+  }
+
+  Template.main.createquizz = function(){
+    return Meteor.user().position == 'quizzcreation';
+  }
+
+  Template.main.answerquizz = function(){
+    return Meteor.user().position == 'quizz';
+  }
+
+  Template.newquestion.events({
+    'click a.new-question' : function(event)
+    {
+      event.preventDefault();
+
+    }
+  })
 }
 
--- server/swxserver.js
+++ server/swxserver.js
@@ -1,18 +1,19 @@
+Colors = new Meteor.Collection("colors");
 if (Meteor.isServer) {
+	Students = new Meteor.Collection("Students");
 	Questions = new Meteor.Collection("questions");
-
-	
 
 	Meteor.startup(function () {
 		Meteor.publish("userData", function()
 		{
 			return Meteor.users.find({_id : this.userId},
-								{fields: {'cv':1}});
+								{fields: {'cv':1, 'position':1}});
 		});
 	});
 
 	Accounts.onCreateUser(function(options, user){
 		user.cv = "";
+		user.position = "welcome";
 		if (options.profile)
 			user.profile = options.profile;
 		return user;
@@ -29,8 +30,11 @@ if (Meteor.isServer) {
 		addCV : function(linkCVtext){
 			Meteor.users.update({_id : Meteor.userId()},
 						{$set: {cv:linkCVtext}});
-			console.log(Meteor.user());
-		}	
-	})
+		},
+		changePos : function(newpos){
+			Meteor.users.update({_id : Meteor.userId()},
+						{$set: {position : newpos}});
+		}
+	});
 
 }
